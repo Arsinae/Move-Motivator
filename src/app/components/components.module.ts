39c44ea402affd4389c9696becd0978607c1bccc
Thibaud Huber <thibaud_huber@yahoo.fr>
2023-11-01 21:41:48 +0100

    chores: move components in sub-directory

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,13 +1,13 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderBarComponent } from './header-bar/header-bar.component';
+import { HeaderBarComponent } from './core/header-bar/header-bar.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { DynamicLocaleId } from '@app/utils/dynamic-locale';
-import { SideBarComponent } from './side-bar/side-bar.component';
+import { SideBarComponent } from './core/side-bar/side-bar.component';
 import { MatListModule } from '@angular/material/list';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ModalsModule } from '../modals/modals.module';
