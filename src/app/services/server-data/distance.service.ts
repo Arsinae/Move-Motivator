a24f2f9c8644a1c4d81066df848ef0b5ec4260ad
Thibaud Huber <thibaud_huber@yahoo.fr>
2023-11-02 21:58:32 +0100

    feat: use firestore data converter

--- src/app/services/server-data/distance.service.ts
+++ src/app/services/server-data/distance.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collection, query, where, collectionData, orderBy } from '@angular/fire/firestore';
+import { Firestore, collection, query, where, collectionData, FirestoreDataConverter, CollectionReference, DocumentData, QueryDocumentSnapshot } from '@angular/fire/firestore';
 import { Activity } from '@app/models/activity';
 import { map, Observable } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
@@ -9,16 +9,32 @@ import { AuthService } from '../auth/auth.service';
 })
 export class DistanceService {
 
+  private collectionRef: CollectionReference<DocumentData>;
+  public converter: FirestoreDataConverter<Activity>;
+
   constructor(
     private firestore: Firestore,
     private authService: AuthService
-  ) { }
+  ) {
+    this.collectionRef = collection(this.firestore, 'user', this.authService.getCurrentUser().uuid, 'distances');
+    this.converter = {
+      toFirestore: (data) => data,
+      fromFirestore: (snap: QueryDocumentSnapshot) => {
+        const data = snap.data()
+        return {...data, creationDate: (<any>data).creationDate.toDate()} as Activity;
+      }
+    };
+  }
 
   getTodayDistances(): Observable<Activity[]> {
     const today = new Date();
     today.setHours(0, 0, 0, 0);
-    const distanceCollection = collection(this.firestore, 'user', this.authService.getCurrentUser().uuid, 'distances');
-    const distanceQuery = query(distanceCollection, where('creationDate', '>=', today));
+    const distanceQuery = query(this.collectionRef, where('creationDate', '>=', today));
+    return collectionData(distanceQuery, { idField: 'id' }) as Observable<Activity[]>;
+  }
+
+  getDistancesBetweenDates(startDate: Date, endDate: Date): Observable<Activity[]> {
+    const distanceQuery = query(this.collectionRef, where('creationDate', '>=', startDate), where('creationDate', '<=', endDate)).withConverter(this.converter);
     return collectionData(distanceQuery, { idField: 'id' }) as Observable<Activity[]>;
   }
 }
