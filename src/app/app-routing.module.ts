7469169d5f2dd4724100036900e5b41529d0e7b7
Thibaud Huber <thibaud_huber@yahoo.fr>
2023-11-08 22:17:30 +0100

    build: use routing modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { AuthGuard, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { AuthActionComponent } from './pages/login/auth-action/auth-action.component';
-import { LoginComponent } from './pages/login/login.component';
 
 const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['/']);
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'auth/action', component: AuthActionComponent },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin } },
+  { path: 'auth/action', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule), canActivate: [AuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin } },
   { path: '',   redirectTo: 'login', pathMatch: 'full' },
-  { path: '**', component: LoginComponent }
+  { path: '**', redirectTo: 'login' }
 ];
 
 @NgModule({
