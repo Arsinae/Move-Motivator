9a5b83fc8a702c7e64c8044d3876b885c37250a4
Thibaud Huber <thibaud_huber@yahoo.fr>
2024-01-04 10:34:23 +0100

    feat: available points

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { GameUserState } from '@app/models/game/game-user-state';
 import { Place } from '@app/models/game/places';
 import { AuthService } from '@app/services/auth/auth.service';
 import { GameStateService } from '@app/services/server-data/game/game-state.service';
 import { PlacesService } from '@app/services/server-data/places/places.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements OnInit, OnDestroy {
 
   public gameState: GameUserState = null;
   public points: Place[] = [];
+
+  private pointsSubscription: Subscription = null;
 
   constructor(
     private authService: AuthService,
@@ -24,7 +27,19 @@ export class GameComponent implements OnInit {
   ngOnInit(): void {
     this.authService.getCurrentUserObservable().subscribe(async res => {
       this.gameState = (await this.gameStateService.getUserGameState());
-      this.placeService.getPlaces().subscribe(res => {
+      this.getPoints();
+    });
+  }
+
+  ngOnDestroy(): void {
+      if (this.pointsSubscription) {
+        this.pointsSubscription.unsubscribe();
+      }
+  }
+
+  public getPoints(): void {
+    this.pointsSubscription = this.gameStateService.getAvailablePoints().subscribe(availablePoints => {
+      this.placeService.getPlaces(availablePoints.map(point => point.id)).subscribe(res => {
         this.points = res;
       });
     });
