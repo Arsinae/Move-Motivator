edf0c3b39fb3891763002ecdb841266cae2676e4
Thibaud Huber <thibaud_huber@yahoo.fr>
2024-01-10 16:57:28 +0100

    feat: get default place dialog

--- functions/src/game/game.ts
+++ functions/src/game/game.ts
@@ -8,3 +8,12 @@ export function movePlayer(request: CallableRequest, firestore: Firestore) {
   firestore.collection('game-state').doc(`${request.auth?.uid}`).update({distance: FieldValue.increment(distance), currentPlace: place});
   return request.auth?.uid;
 }
+
+export async function getPlaceDialog(request: CallableRequest, firestore: Firestore) {
+  const { place } = request.data;
+  logger.info(`Get Dialog = User ${request.auth?.uid}, Place: ${place}, `);
+  // const userGameState = await firestore.collection('game-state').doc(request.auth?.uid).get();
+  const placeDialogs = (await firestore.collection(`places/${place}/dialog`).get()).docs;
+  logger.info(`Result Dialog List: ${placeDialogs?.length} entry; ${JSON.stringify(placeDialogs.map(dialog => dialog.data()))}`);
+  return placeDialogs.map(dialog => dialog.data());
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,7 @@ import {onCall} from "firebase-functions/v2/https";
 import {onDocumentCreated} from 'firebase-functions/v2/firestore';
 import * as admin from 'firebase-admin';
 import { aggregateDistanceInfos, storeDistance, recalculateDailyAverage } from "./distance/distance";
-import { movePlayer } from './game/game';
+import { getPlaceDialog, movePlayer } from './game/game';
 
 admin.initializeApp();
 
@@ -22,4 +22,8 @@ exports.computeDailyAverage = onCall((request) => {
 
 exports.movePlayer = onCall((request) => {
   movePlayer(request, admin.firestore());
+});
+
+exports.getDialogs = onCall(async (request) => {
+  return await getPlaceDialog(request, admin.firestore());
 });
